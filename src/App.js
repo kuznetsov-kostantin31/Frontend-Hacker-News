62d57a7a63bb48acf0b06a4a7ba5cdaad0af3ecc
Kuznetsov Konstantin <kuznetsovkonst31@gmail.com>
2024-02-23 22:20:08 +1000

    Upd

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React, {useState} from "react";
-
+import React, {useMemo, useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
-    const [likes, setLikes] = useState(0)
-    const [value, setValue] = useState('ТЕКСТ В ИНПУТ')
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'aa', body: 'zz'},
+        {id: 2, title: 'bb', body: 'yy'},
+        {id: 3, title: 'cc', body: 'xx'}
+    ])
+    const [filter, setFilter] = useState({sort: '', query: ''})
 
-    function increment(){
-        setLikes(likes + 1)
+    const sortedPosts = useMemo(()=>{
+        if(filter.sort){
+            return [...posts].sort((a, b)=> a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts
+    }, [filter.sort, posts])
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
-    function decrement(){
-        setLikes(likes - 1)
+
+    const sortedAndSearchedPosts = useMemo(()=>{
+        return sortedPosts.filter(post=>post.title.toLowerCase().includes(filter.query))
+    }, [filter.query, sortedPosts])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
     }
-  return (
-      <div className="App">
-          <h1>{likes}</h1>
-          <h1>{value}</h1>
-          <input
-              type="text"
-              value={value}
-              onChange={event => setValue(event.target.value)}
-          />
-          <button onClick={increment}>Increment</button>
-          <button onClick={decrement}>Decrement</button>
-      </div>
-  );
+
+    return (
+        <div className="App">
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <PostList remove = {removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
+        </div>
+    );
 }
 
 export default App;
